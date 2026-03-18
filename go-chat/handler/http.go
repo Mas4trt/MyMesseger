ce7a1992c4ca687c1871ae3a1de3de48d1cd5ca5
Yaroslav <fokrivosheev@mail.ru>
2026-03-18 18:46:36 +0500

    Написан handler

---
+++ go-chat/handler/http.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"encoding/json"
+	"go-chat/room"
+	"net/http"
+)
+
+type CreateRoomResponse struct {
+	Token string `json:"token"`
+}
+
+func CreateRoomHandler(manager *room.Manager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		room := manager.CreateRoom()
+
+		w.Header().Set("Conect-Type", "application/json")
+
+		json.NewEncoder(w).Encode(CreateRoomResponse{
+			Token: room.Token,
+		})
+	}
+}
---
+++ go-chat/handler/ws.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"go-chat/room"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func WebSocketHandler(manager *room.Manager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.URL.Query().Get("token")
+		nickname := r.URL.Query().Get("nickname")
+
+		rm := manager.GetRoom(token)
+		if rm == nil {
+			http.Error(w, "Команата не найдена", http.StatusNotFound)
+			return
+		}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		client := &room.Client{
+			Nickname: nickname,
+			Conn:     conn,
+		}
+
+		rm.Clients[client] = true
+
+		defer func() {
+			delete(rm.Clients, client)
+			conn.Close()
+		}()
+
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			fullMessege := client.Nickname + ": " + string(msg)
+
+			for c := range rm.Clients {
+				c.Conn.WriteMessage(websocket.TextMessage, []byte(fullMessege))
+			}
+		}
+	}
+}
